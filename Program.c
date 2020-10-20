1e9bb51bc81a880bebe40d3134490970aeb38a56
Savindu Bandara <36003016+bsavindu1998@users.noreply.github.com>
2020-10-20 13:29:08 +0530

    Update Program.c
    Added Few Modifications and fixed errors

--- Program.c
+++ Program.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include<conio.h>
 
 int main()
 {
-    char name,country,skill;
+    char skill;
+    char name[100];
+    char country[100];
+    int age=0;
+
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    fgets(name, 100, stdin);
     printf("Enter your Country of Residance : ");//Get user country
-    scanf("%s",&country);
+    fgets(country, 100, stdin);
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    scanf("%d",&age);
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    printf("Thank you %s from %s Happy Hacking",name,country);//Thank the user
 
     return 0;
 }
